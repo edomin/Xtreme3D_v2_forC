f57ac8a9d9e71c0b542d2e9fd544af55d553f082
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-04-26 03:05:02 +0600

    window and timer functions moved to individual headers

---
+++ include/timer.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+void X3D_TimerStart(void);
+uint32_t X3D_TimerDelay(uint32_t ms);
+uint32_t X3D_TimerDelayForFPS(uint32_t fps);
---
+++ include/window.h
@@ -0,0 +1,5 @@
+#include <windows.h>
+
+HWND X3D_WindowCreate(int width, int height, int depth, BOOL fullscreen, char *caption);
+int X3D_WindowDestroy(HWND hwnd);
+BOOL X3D_WindowXed();
